d5e92bdba546c4b423a699cfa6496fb16e2db2fb
Igor Tchone Riba <132633397+IgorTchone@users.noreply.github.com>
2024-12-14 17:40:12 +0000

    Add files via upload

---
+++ ValueOfI.java
@@ -0,0 +1,10 @@
+public class ValueOfI {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("The value of i is: " + i);
+        }
+    }
+
+
+}
